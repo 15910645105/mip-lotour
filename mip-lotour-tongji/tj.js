f41dd5520566c2b55e9594232d8c9cc10a0a0281
15910645105 <258269163@qq.com>
2017-01-06 10:26:34 +0800

    Create tj.js

---
+++ mip-lotour-tongji/tj.js
@@ -0,0 +1,21 @@
+var  _gaq  =  _gaq  ||  [];
+        _gaq.push(['_setAccount',  'UA-39387460-4']);
+        _gaq.push(['_setDomainName',  'lotour.com']);
+        _gaq.push(['_addOrganic',  'baidu',  'word']);
+        _gaq.push(['_addOrganic',  'baidu',  'wd']);
+        _gaq.push(['_addOrganic',  'soso',  'query']);     
+        _gaq.push(['_addOrganic',  'soso',  'w']);
+        _gaq.push(['_addOrganic',  '3721',  'name']);
+        _gaq.push(['_addOrganic',  'yodao',  'q']);
+        _gaq.push(['_addOrganic',  'vnet',  'kw']);
+        _gaq.push(['_addOrganic',  'sogou',  'query']);
+        _gaq.push(['_addOrganic',  '360',  'q']);
+        _gaq.push(['_addOrganic',  'so',  'q']);
+        var  full_url  =  location.pathname  +  location.search  +  location.hash;
+        _gaq.push(['_trackPageview',  full_url]);
+        _gaq.push(['_trackPageLoadTime']);
+        (function  ()  {
+                var  ga  =  document.createElement('script');  ga.type  =  'text/javascript';  ga.async  =  true;
+                ga.src  =  ('https:'  ==  document.location.protocol  ?  'https://ssl'  :  'http://www')  +  '.google-analytics.com/ga.js';
+                var  s  =  document.getElementsByTagName('script')[0];  s.parentNode.insertBefore(ga,  s);
+        })();
